5c778891be933524421658f8d86e0b9796a18919
Sajid2210 <puthawalasajid5@gmail.com>
2025-08-29 23:47:44 +0530

    bugs fix

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os, sys
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
-SRC_DIR = BASE_DIR / "src"          # local me yeh exist nahi hota
+SRC_DIR = BASE_DIR / "src"
 if SRC_DIR.exists() and str(SRC_DIR) not in sys.path:
     sys.path.insert(0, str(SRC_DIR))
 
